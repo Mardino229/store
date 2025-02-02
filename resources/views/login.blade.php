dabd6651c6b839a737eeba289dbc77aed0d15e5d
Mardino229 <maxdassi01@gmail.com>
2025-02-02 10:46:40 +0100

    Delployment

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -55,7 +55,7 @@
             </div>
             <div>
                 <div class="flex items-center justify-between">
-                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Passworde</label>
+                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Passwordes</label>
                     <div class="text-sm">
                         <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                     </div>
